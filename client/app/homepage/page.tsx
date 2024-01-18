d350802a14a4007d079f4c84276d97c524a3f1f2
Anusha Chitranshi <chitraaa@mail.uc.edu>
2024-01-18 14:12:05 -0500

    feat(client): changes to homepage

--- client/app/homepage/page.tsx
+++ client/app/homepage/page.tsx
@@ -19,13 +19,6 @@ function Landing() {
       }}
     >
       <Image
-        style={{
-          width: 1440,
-          height: 832,
-          //   left: -30,
-          //   top: -16,
-          position: "relative",
-        }}
         src="https://images.unsplash.com/photo-1532712938310-34cb3982ef74?auto=format&fit=crop&q=80&w=2940&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         alt="Loading..."
         width={1440}
