512b05a3b1066bd4eb1149780eb33c735226a4e3
Zenith Zack <faldund@outlook.com>
2024-02-08 13:51:22 -0500

    feat(client): add a payments table component

--- src/app/settings/transactions/page.tsx
+++ src/app/settings/transactions/page.tsx
@@ -1,10 +1,19 @@
+import { PaymentsDataTable } from "@/app/_components/Account/PaymentsDataTable";
+import { Separator } from "@/components/ui/separator";
+
+
 
 export default function TransactionsPage() {
   return (
-    <div>
-      <p className="text-muted-foreground">
-        Manage your account settings and set e-mail preferences.
-      </p>
+    <div className="space-y-6">
+      <div>
+        <h3 className="text-lg font-medium">Transactions</h3>
+        <p className="text-sm text-muted-foreground">
+          View your past transactions
+        </p>
+      </div>
+      <Separator />
+      <PaymentsDataTable/>
     </div>
   );
 }
